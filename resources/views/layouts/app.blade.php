a7653f119e38647f7d856e65973939dbd285278a
dharmeshpaliwalcodepranetra-bit <dharmeshpaliwal.codepranetra@gmail.com>
2026-05-07 19:08:22 +0530

    Added the script tag given by client

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,6 +30,15 @@
         <style>
 
         </style>
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-RGSR4TRM74"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+   gtag('js', new Date());
+
+  gtag('config', 'G-RGSR4TRM74');
+</script>
     </head>
     <body>
         <!-- Navigation -->
